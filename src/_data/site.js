e3c9b94c110667e460cc314fd671c142b491ca7a
masaiful <43263643+masaiful@users.noreply.github.com>
2023-08-07 12:22:03 +0700

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,14 +1,14 @@
 module.exports = {
   env: process.env.ELEVENTY_ENV,
-  title: "Bob Monsour | Home",
+  title: "Saiful Muhajir | Home",
   description:
-    "The personal website of Bob Monsour. Enjoying learning and deploying websites with 11ty and Netlify.",
-  url: "https://www.bobmonsour.com",
+    "Saiful Muhajir's viewpoint, mostly about database technology",
+  url: "https://saifulmuhajir.web.id",
   language: "en",
   author: {
-    name: "Bob Monsour",
-    email: "bob.monsour@gmail.com",
-    url: "https://bobmonsour.com/",
+    name: "Saiful Muhajir",
+    email: "saifulmuhajir@gmail.com",
+    url: "https://saifulmuhajir.web.id/",
   },
   mainNavLinks: [
     { url: "/archive/", text: "Archive" },
